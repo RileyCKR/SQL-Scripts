35b66f13a4e66e74bbc56d3dda94c804d00095fa
Wydra <RileyCKR@gmail.com>
2013-02-24 13:57:59 -0500

    adding comments

--- FixAlias.sql
+++ FixAlias.sql
@@ -1,13 +1,18 @@
-
+/*
+Rebuilds Kentico's node alias paths if the data has become out of
+sync or corrupted.
+*/
 DECLARE @aliasLevel INT = 0
 DECLARE @top INT
 SELECT @top = MAX([NodeLevel]) FROM [CMS_Tree]
 
+--Loop from root of tree to top
 WHILE (@aliasLevel < @top)
 BEGIN
 
     SET @aliasLevel = @aliasLevel + 1
     
+    --Update alias path of nodes at this level
     UPDATE node
        SET node.[NodeAliasPath] = parent.[NodeAliasPath] + '/' + node.[NodeAlias]
       FROM [CMS_Tree] node
