4eabffb84dd878d934cd099bab69cb4d6abc35fa
Wydra <RileyCKR@gmail.com>
2013-02-24 13:50:25 -0500

    Adding MS style square brackets

--- FixAlias.sql
+++ FixAlias.sql
@@ -1,21 +1,21 @@
 
 DECLARE @aliasLevel INT = 0
 DECLARE @top INT
-SELECT @top = MAX(NodeLevel) FROM CMS_Tree
+SELECT @top = MAX([NodeLevel]) FROM [CMS_Tree]
 
 WHILE (@aliasLevel < @top)
 BEGIN
 	SET @aliasLevel = @aliasLevel + 1
 	
 	UPDATE node
-	SET node.NodeAliasPath = parent.NodeAliasPath + '/' + node.NodeAlias
-	FROM CMS_Tree node
-	INNER JOIN CMS_Tree parent
-	ON node.NodeParentID = parent.NodeID
-	WHERE node.NodeLevel = @aliasLevel
+	SET node.[NodeAliasPath] = parent.[NodeAliasPath] + '/' + node.[NodeAlias]
+	FROM [CMS_Tree] node
+	INNER JOIN [CMS_Tree] parent
+	ON node.[NodeParentID] = parent.[NodeID]
+	WHERE node.[NodeLevel] = @aliasLevel
 	
 END
 
 --Clean up double // from beginning of alias paths
-UPDATE CMS_Tree set NodeAliasPath = REPLACE(NodeAliasPath, '//', '/')
+UPDATE [CMS_Tree] SET [NodeAliasPath] = REPLACE([NodeAliasPath], '//', '/')
 GO
