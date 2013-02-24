885d3282030a623acd2c754cfc2a050295819764
Wydra <RileyCKR@gmail.com>
2013-02-24 13:53:48 -0500

    Formatting whitespace

--- FixAlias.sql
+++ FixAlias.sql
@@ -5,15 +5,16 @@ SELECT @top = MAX([NodeLevel]) FROM [CMS_Tree]
 
 WHILE (@aliasLevel < @top)
 BEGIN
-	SET @aliasLevel = @aliasLevel + 1
-	
-	UPDATE node
-	SET node.[NodeAliasPath] = parent.[NodeAliasPath] + '/' + node.[NodeAlias]
-	FROM [CMS_Tree] node
-	INNER JOIN [CMS_Tree] parent
-	ON node.[NodeParentID] = parent.[NodeID]
-	WHERE node.[NodeLevel] = @aliasLevel
-	
+
+    SET @aliasLevel = @aliasLevel + 1
+    
+    UPDATE node
+       SET node.[NodeAliasPath] = parent.[NodeAliasPath] + '/' + node.[NodeAlias]
+      FROM [CMS_Tree] node
+INNER JOIN [CMS_Tree] parent
+        ON node.[NodeParentID] = parent.[NodeID]
+     WHERE node.[NodeLevel] = @aliasLevel
+    
 END
 
 --Clean up double // from beginning of alias paths
